8058b8d8cb939da381b0005c0b5f62c179db7db9
Daahir Mohamed Nuur <daahir_mohamed.nuur0003@stud.hkr.se>
2025-02-09 14:37:55 +0100

    Added two files now

---
+++ Main.java
@@ -0,0 +1,7 @@
+// Main class to test the Car class
+public class Main {
+    public static void main(String[] args) {
+        // Corrected line with a semicolon
+        System.out.println("Hello, this is the first class in Java!");
+    }
+}
---
+++ Untitled.java
@@ -0,0 +1,11 @@
+ 
+// Main class to test the Car class
+public class Main {
+    public static void main(String[] args) {
+        // Creating an object of the Car class
+        Car car1 = new Car("Toyota Corolla", 2020);
+
+        // Calling the method to display car information
+        car1.displayInfo();
+    }
+}
